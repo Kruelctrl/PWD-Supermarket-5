eb911a718e6eceb305092659a166479d95415e0e
anjashardian <anjashardian@gmail.com>
2021-03-24 11:42:42 +0700

    register hampir done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -61,6 +61,126 @@ module.exports = {
             return res.status(500).send(error);
         }
     },
+    keepLogin: async (req, res) => {
+        try {
+            //get user data
+            const getUser = `select * from user
+                            where username=${db.escape(req.user.username)}`
+
+            const result = await asyncQuery(getUser)
+            console.log(result)
+
+            res.status(200).send(result[0])
+        }
+        catch (error) {
+            return res.status(500).send(error)
+        }
+    },
+
+    register: async (req, res) => {
+        const { username, password, email } = req.body
+        // NOTE error tertampung di validation result, ditampung krn nanti mau di cek
+        // NOTE VALIDATION INPUT FROM USERS 
+        const errors = validationResult(req)
+        // console.log(errors)
+
+        if (!errors.isEmpty()) return res.status(400).send(errors.array()[0].msg)
+
+        // REVIEW
+        // NOTE ENCRYPT PASS WITH CRYPTO JS
+        // NOTE data yg sudah di encrypt oleh crypto js tidak bisa didecrypt
+        const hashpass = cryptoJS.HmacMD5(password, SECRET_KEY)
+
+        // NOTE untuk lihat bentuknya hashpass
+        // console.log('haspass', hashpass.toString())
+
+        try {
+            // NOTE kalau tidak ada error, baru proses penambahan data user baru berjalan
+            const queryCheckReg = `select * from user 
+                                where username=${db.escape(username)}
+                                or email=${db.escape(email)}`
+
+            const resultCheckReg = await asyncQuery(queryCheckReg)
+
+            if (resultCheckReg.length !== 0) return res.status(400).send('USERNAME OR EMAIL ALREADY EXIST')
+
+            const queryReg = `insert into user (username, email, password)
+                            values (${db.escape(username)}, ${db.escape(email)}, ${db.escape(hashpass.toString())})`
+
+            const resultReg = await asyncQuery(queryReg)
+
+
+
+            // NOTE create token kalo berhasil verify
+            // NOTE insertId dapet dari postman pas register data baru
+            const token = createToken({ id: resultReg.insertId, username: username })
+
+            // sent email verification to user
+            const option = {
+                from: `admin <anjashardian59@gmail.com>`,
+                to: `anjashardian@gmail.com`,
+                subject: "FAT Verification",
+                text: `Hello our precious customers,
+        
+                Click link below to verified your account
+        
+                Your PARCEL best provider,
+                  FAT PARCEL.`,
+                html: `
+                <a href ="http://localhost:2000/verification/${token}">http://localhost/verification/${token}</a>`,
+            };
+            const info = await transporter.sendMail(option);
+            console.log(req.body);
+
+            res.status(200).send(info.response)
+        }
+        catch (err) {
+            console.log(err)
+            res.status(400).send(err)
+        }
+
+
+
+        // db.query(queryCheckReg, (err, result) => {
+        //     // NOTE cek error
+        //     if (err) return res.status(500).send(err)
+
+        //     // NOTE cek apakah di dlm database ada usernam atau email yang sama
+        //     if (result.length !== 0) return res.status(400).send('USERNAME OR EMAIL ALREADY EXIST')
+
+        //     const queryReg = `insert into users (username, email, password)
+        //                      values (${db.escape(username)}, ${db.escape(email)}, ${db.escape(hashpass.toString())})`
+
+        //     db.query(queryReg, (err2, result2) => {
+        //         // NOTE cek error
+        //         if (err2) return res.status(500).send(err2)
+
+        //         res.status(200).send(result2)
+        //     })
+        // })
+
+        // NOTE ini buat ngetes haspass, si dbquery diatas di comment dulu trs jalanin si res.status test nya
+        // res.status(200).send('test hashpassword')
+    },
+
+    emailVerification: async (req, res) => {
+        try {
+            // change status user in database
+            const qUpdateStatus = `UPDATE user SET status = verified WHERE id_user = ${req.user.id}`;
+            const updateStatus = await asyncQuery(qUpdateStatus);
+
+            const getUser = `select * from user where id_user = ${req.user.id}`;
+            const result = await asyncQuery(getUser);
+
+            delete result[0].password;
+
+            res.status(200).send(`Congratulations! Your account has been verified`);
+        }
+        catch (err) {
+            console.log(err)
+            res.status(400).send(err)
+        }
+    }
     
 
 
---
+++ frontend/src/pages/verification.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Axios from "axios";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+
+class Verification extends React.Component {
+  async componentDidMount() {
+    const token = this.props.location.search.substring(1);
+
+    try {
+      const res = await Axios.post("http://localhost:2000/user/verification", {
+        token,
+      });
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  render() {
+    return (
+      <div style={styles.container}>
+        <h3 style={{ textAlign: "center" }}>Your Account Has Been Verified</h3>
+        <div style={{ textAlign: "center" }}>
+          <Button as={Link} to="/login">
+            Login
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+const styles = {
+  container: {
+    margin: "auto",
+    height: 400,
+    width: 500,
+    alignItems: "center",
+    marginTop: 200,
+  },
+};
+export default Verification;
